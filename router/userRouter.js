aaba99c02a25800a7047cb457f5ac40410323535
assyahame <ahamedashiq08@gmail.com>
2023-01-13 12:19:55 +0530

    validate email

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createUser, loginUser, forgotPassword, resetUserPassword, getUser } = require('../controller/user')
+const { createUser, loginUser, forgotPassword, resetUserPassword, getUser, uniqueEmail } = require('../controller/user')
 const userRouter = express.Router()
 
 userRouter.route('/user/:id').get(getUser)
@@ -7,5 +7,6 @@ userRouter.route('/user/register').post(createUser)
 userRouter.route('/user/password/forgot').post(forgotPassword)
 userRouter.route('/user/password/reset/:token').post(resetUserPassword)
 userRouter.route('/user/login').post(loginUser)
+userRouter.route('/user/:email').get(uniqueEmail)
 
 module.exports = userRouter
